b615f3232e0e76aecb256d4442d7da8fbfa23126
Julian Mahecha <jmahecha@belatrixsf.com>
2018-08-29 10:51:53 -0500

    add some test for routing

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -2,18 +2,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 import { UserDetailsComponent } from './user-details.component';
+import { Observable, Subject } from 'rxjs';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub {
+  navigate(params) {
+
+  }
+}
+
+class ActivatedRouteStub {
+  private subject = new Subject();
+
+  get params() {
+    return this.subject.asObservable();
+  }
+
+  push(value) {
+    this.subject.next(value);
+  }
+
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [UserDetailsComponent],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,4 +49,22 @@ xdescribe('UserDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should redirect the user to users page after saving', () => {
+    const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+    component.save();
+    expect(spy).toHaveBeenCalledWith(['users']);
+  });
+
+  it('should navigate the user to the not found page when an invalid user id is passed', () => {
+    const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+
+    const route: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+    route.push({ id: 0 });
+
+    expect(spy).toHaveBeenCalledWith(['not-found']);
+  });
+
 });
