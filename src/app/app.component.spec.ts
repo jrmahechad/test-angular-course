89e8e458113b0b4f9fe7390d7b70265f687b4bbe
Julian Mahecha <jmahecha@belatrixsf.com>
2018-08-29 11:03:09 -0500

    update angular cli and add more tests for routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -14,7 +15,8 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes([])],
-      declarations: [AppComponent]
+      declarations: [AppComponent, NavComponent],
+      // schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   }));
@@ -30,9 +32,5 @@ describe('AppComponent', () => {
     expect(de).not.toBeNull();
   });
 
-  it('should have a link to todos page', () => {
-    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
-    const index = debugElements.findIndex(de => de.properties['href'] === '/todos');
-    expect(index).toBeGreaterThan(-1);
-  });
+
 });
